7ca4c3f388b3d31453c00f5b716ec0e1f5ca9f74
raj <raj@gmail.com>
2025-07-17 16:16:35 +0530

    Scripting practise

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+name=$1
+date
+echo "Hi $name"
+echo "this is 1st argument $1"
+
+echo "all arguments $@"
+echo "How are you!"
+read ans
+if [ $ans == Fine ]; then
+	echo "Good to hear!"
+else
+	echo "oh! sorry. you will be fine"
+fi
+echo "How old are you?"
+read age
+if [ $age -gt 18 ]; then
+	echo "Good! You are major"
+else
+	echo "Oh!. you are kid"
+fi
+echo "what is in your bag? show it one by one"
+sleep 5
+for file in *; do
+	echo "this is $file"
+done
+echo "i feel bore!"
+echo "lets play a game"
+echo "guess a number that i wrote on my hand"
+num=1
+while [ "$num" -ne 0 ]; do
+	echo "------------------------------"
+	echo "common! Try it"
+	echo "Enter a number:"
+	read num
+	
+	
+
+done
+echo "superrb..! you are very intelligent"
