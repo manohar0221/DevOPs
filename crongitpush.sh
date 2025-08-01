1a494f8cf4aac26e739d95de84e85be7c954e89e
raj <raj@gmail.com>
2025-08-01 17:02:48 +0530

    Appending commands history on 2025-08-01 17:02:48 env:dev

--- crongitpush.sh
+++ crongitpush.sh
@@ -3,7 +3,7 @@
 cat ~/.bash_history >> commandshistory.txt
 
 # Always try to add & commit
-git add -u commandshistory.txt
+git add -A commandshistory.txt crongitpush.sh helloworld/ 
 if git commit -m "Appending commands history on $(date '+%Y-%m-%d %H:%M:%S') env:dev"; then
     git push origin master
     echo "History updated and pushed."
